067b242e883c3bc3f4d33eed8a289413738694c2
rafael-puelma <rafael.puelma@outlook.com>
2020-10-22 15:30:51 +0200

    Finished app!

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 // variabler
 let calculate = "";
 
+// Get needed elements.
 const equal = document.querySelector(".btn-equal");
 const clear = document.querySelector(".btn-clear");
-const operation = document
-  .querySelector(".btn-yellow")
-  .getAttribute("data-num");
 const button = document.querySelectorAll(".btn");
-console.log(operation);
 
 // hämta siffror
 button.forEach((item) => {
   item.addEventListener("click", (event) => {
     calculate = calculate + item.getAttribute("data-num");
-    console.log(calculate);
     document.querySelector(".screen").value = calculate;
   });
 });
 
+// Evaluates sum.
 equal.addEventListener("click", (event) => {
   calculate = eval(calculate).toString();
 
   document.querySelector(".screen").value = calculate;
-  calcutale = "";
+  calculate = "";
 });
 
+// Clears screen.
 clear.addEventListener("click", (event) => {
   document.querySelector(".screen").value = "";
   calculate = "";
